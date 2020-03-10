22106c767fd941fb2719c99d611221f3c1fbe115
aktddsilva <aktddsilva@gmail.com>
2020-03-10 15:59:42 +0800

    FES Automation v1.44 - added delay to flex sensor reading and changed stimulation output from digital to analog

--- main.cpp
+++ main.cpp
@@ -66,14 +66,14 @@ HDWF hdwf;
 double hzSys;
 char szError[512] = { 0 };
 char szDeviceName[32];
-int dgIO1 = 4; // set digital pin 1
-int dgIO2 = 6; // set digital pin 2
-int dFreq = 10000;
-int dDutyPlus = 0;
-int dSamples = 100;
-int cSamples;
 
-
+//analog signal settings
+int analogChannel = 0;
+double aCarrierFreq = 10e3;
+double aCarrierAmp = 1.65;
+double aAMFreq = 50.0;
+int aAMAmp = 100;
+int aAMOffset = -100;
 
 // class for getting x and y values in UI function
 selectionMatrix choice;
@@ -136,18 +136,18 @@ void initAD()
 	FDwfAnalogIOChannelNodeSet(hdwf, 0, 0, 1);
 	FDwfAnalogIOChannelNodeSet(hdwf, 0, 1, SUPPLY_VOLTAGE);
 
-	// configure 10kHz custom on IO pin 0
-	FDwfDigitalOutEnableSet(hdwf, dgIO1, true);
-    FDwfDigitalOutOutputSet(hdwf, dgIO1, DwfDigitalOutOutputThreeState);
-    FDwfDigitalOutTypeSet(hdwf, dgIO1, DwfDigitalOutTypeCustom);
-    FDwfDigitalOutIdleSet(hdwf, dgIO1, DwfDigitalOutIdleZet);
-    FDwfDigitalOutDividerSet(hdwf, dgIO1, hzSys / dFreq / 2);
-    FDwfDigitalOutDataSet(hdwf, dgIO1, rgcustom, 100 * 8);
-    
-    // configure 3.3V on IO pin 1
-    FDwfDigitalOutEnableSet(hdwf, dgIO2, true);
-    FDwfDigitalOutDividerSet(hdwf, dgIO2, hzSys / dFreq / dSamples);
-    FDwfDigitalOutCounterSet(hdwf, dgIO2, 0, 100);
+	FDwfAnalogOutCustomAMFMEnableSet(hdwf, analogChannel, true);
+    FDwfAnalogOutNodeEnableSet(hdwf, analogChannel, AnalogOutNodeCarrier, true);
+    FDwfAnalogOutNodeFunctionSet(hdwf, analogChannel, AnalogOutNodeCarrier, funcSquare);
+    FDwfAnalogOutNodeAmplitudeSet(hdwf, analogChannel, AnalogOutNodeCarrier, aCarrierAmp);
+    FDwfAnalogOutNodeFrequencySet(hdwf, analogChannel, AnalogOutNodeCarrier, aCarrierFreq);
+    FDwfAnalogOutNodeEnableSet(hdwf, analogChannel, AnalogOutNodeAM, true);
+    FDwfAnalogOutNodeFunctionSet(hdwf, analogChannel, AnalogOutNodeAM, funcPulse);
+    FDwfAnalogOutNodeAmplitudeSet(hdwf, analogChannel, AnalogOutNodeAM, aAMAmp);
+    FDwfAnalogOutNodeFrequencySet(hdwf, analogChannel, AnalogOutNodeAM, aAMFreq);
+    FDwfAnalogOutNodeOffsetSet(hdwf, analogChannel, AnalogOutNodeAM, aAMOffset);
+    FDwfAnalogOutConfigure(hdwf, analogChannel, true);
+
     
     // enable both analog channels
     FDwfAnalogInChannelEnableSet(hdwf, 0, true);
@@ -195,12 +195,11 @@ int dataGatheringSession(int x, int y)
 	xy(0,13);cout << "vPot  3:";
 	xy(0,15);cout << "Prob   :";
 	// --- 
-	
 	// turn on master switches on pins
 	ad2_enableMasterSwitches(true);
+	Wait(0.2);
 		
 	while(breakFlag) {
-		
 		// update UI values while !kbhit()
 		while(!kbhit()) {
 			// read raw voltage value from flex sensor
@@ -212,11 +211,11 @@ int dataGatheringSession(int x, int y)
 			
 			if(i < 2) {
 						if(!i){
-							tempVBend = (tempVBend <= vFlex_raw)? tempVBend:vFlex_raw;
+							tempVBend = (tempVBend < vFlex_raw)? tempVBend:vFlex_raw;
 							tempArr[i] = tempVBend;
 						}
-						else{
-							tempVFlat = (tempVFlat >= vFlex_raw)? tempVFlat:vFlex_raw;
+						else if(i){
+							tempVFlat = (tempVFlat > vFlex_raw)? tempVFlat:vFlex_raw;
 							tempArr[i] = tempVFlat;
 						}
 			} else if(i < 3){
@@ -285,7 +284,6 @@ int dataGatheringSession(int x, int y)
 						vPotMatrix[x][y][i] = tempArr[(i * 2) + 4];
 					}
 					trialAverage = (tempArr[3]+tempArr[5]+tempArr[7])/3;
-//					flexProbability[x][y] = (((tempArr[2]-((tempArr[3]+tempArr[5]+tempArr[7])/3))/(tempArr[2]-tempArr[0]))*100);
 					flexProbability[x][y] = (trialAverage<tempArr[2])?((tempArr[2]-trialAverage)/((tempArr[2]-tempArr[0])/100)):((tempArr[2]-trialAverage)/((tempArr[1]-tempArr[2])/100));
 					xy(9,15); cout << flexProbability[x][y] << "\n";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
 					system("pause");
@@ -294,7 +292,6 @@ int dataGatheringSession(int x, int y)
 					} else if(flexProbability[x][y] < 0) {
 						system("msg * Probability is lesser than 0. You might want to retest.");
 					}
-					
 					isElectrodeRead[x][y] = 1;
 					breakFlag = 0;
 				}
